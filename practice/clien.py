2658cb552e89c671104cfc4253d105f15c6afc26
Divyang bhargava <divyang018@gmail.com>
2018-05-24 00:37:22 +0530

    updated

--- practice/clien.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2
-
-import socket,time
-
-x= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-while 3>2:
-	msg= raw_input("Enter message to send")
-	x.sendto(msg,("192.168.10.197",8881)
-	
